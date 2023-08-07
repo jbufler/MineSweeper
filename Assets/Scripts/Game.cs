12cc0cca0820ccf1ac967d036009dcaae00af2e4
jbufler <jbufler@uni-osnabrueck.de>
2023-08-07 19:21:24 +0200

    empty baord and camera position

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    [SerializeField] private int width = 16;
+    [SerializeField] private int height = 16;
+    [SerializeField] private int mineCount = 32;
+
+    private Board board;
+    private Cell[,] state;
+
+    private void Awake()
+    {
+        board = GetComponentInChildren<Board>();
+    }
+
+    private void Start()
+    {
+        NewGame();
+    }
+
+    private void NewGame()
+    {
+        state = new Cell[width, height];
+
+        GenerateCells();
+
+        Camera.main.transform.position = new Vector3(width / 2f, height / 2f, -10f);
+        board.Draw(state);
+    }
+
+    private void GenerateCells()
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                Cell cell = new Cell();
+                cell.position = new Vector3Int(x, y, 0);
+                cell.type = Cell.Type.Empty;
+                state[x, y] = cell;
+            }
+        }
+    }
+}
